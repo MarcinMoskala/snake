1ffb7c3623ccf34ce1c8f66ae392439895cc30a5
marcinmoskala <marcinmoskala@gmail.com>
2022-01-31 18:09:44 +0100

    Update

--- game1.py
+++ game1.py
@@ -21,15 +21,6 @@ def draw_snake_part(pos: Position):
                 CUBE_SIZE,
                 CUBE_SIZE)
     pygame.draw.rect(screen, GREEN, position)
-    pygame.display.update()
-
-
-def draw_food(pos: Position):
-    radius = float(CUBE_SIZE) / 2
-    position = (pos.x * CUBE_SIZE + radius,
-                pos.y * CUBE_SIZE + radius)
-    pygame.draw.circle(screen, BLUE, position, radius)
-    pygame.display.update()
 
 
 def draw_snake(snake: [Position]):
@@ -37,30 +28,35 @@ def draw_snake(snake: [Position]):
         draw_snake_part(element)
 
 
+def draw_food(pos: Position):
+    radius = float(CUBE_SIZE) / 2
+    position = (pos.x * CUBE_SIZE + radius,
+                pos.y * CUBE_SIZE + radius)
+    pygame.draw.circle(screen, BLUE, position, radius)
+
+
 def fill_bg():
     screen.fill(WHITE)
 
 
-snake = [
-    Position(1, 1),
-    Position(1, 2),
-    Position(1, 3),
-    Position(2, 3),
-    Position(2, 4),
-    Position(2, 5),
-]
-
-food = Position(10, 6)
-
-
-def draw():
+def draw(snake, food):
     fill_bg()
     draw_snake(snake)
     draw_food(food)
     pygame.display.update()
 
 
-draw()
+draw(
+    snake=[
+        Position(1, 1),
+        Position(1, 2),
+        Position(1, 3),
+        Position(2, 3),
+        Position(2, 4),
+        Position(2, 5),
+    ],
+    food=Position(10, 6),
+)
 
 while True:
     for event in pygame.event.get():
