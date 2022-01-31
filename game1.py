361b3bf3127fc7c74f0c9424af3e0ee281a62a91
marcinmoskala <marcinmoskala@gmail.com>
2022-01-31 09:22:33 +0100

    Add Game1

---
+++ game1.py
@@ -0,0 +1,70 @@
+import pygame
+
+from Position import Position
+
+pygame.init()
+
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+
+CUBE_SIZE = 25
+CUBES_NUM = 20
+WIDTH = CUBE_SIZE * CUBES_NUM
+screen = pygame.display.set_mode((WIDTH, WIDTH))
+
+
+def draw_snake_part(pos: Position):
+    position = (pos.x * CUBE_SIZE,
+                pos.y * CUBE_SIZE,
+                CUBE_SIZE,
+                CUBE_SIZE)
+    pygame.draw.rect(screen, GREEN, position)
+    pygame.display.update()
+
+
+def draw_food(pos: Position):
+    radius = float(CUBE_SIZE) / 2
+    position = (pos.x * CUBE_SIZE + radius,
+                pos.y * CUBE_SIZE + radius)
+    pygame.draw.circle(screen, BLUE, position, radius)
+    pygame.display.update()
+
+
+def draw_snake(snake: [Position]):
+    for element in snake:
+        draw_snake_part(element)
+
+
+def fill_bg():
+    screen.fill(WHITE)
+
+
+snake = [
+    Position(1, 1),
+    Position(1, 2),
+    Position(1, 3),
+    Position(2, 3),
+    Position(2, 4),
+    Position(2, 5),
+]
+
+food = Position(10, 6)
+
+
+def draw():
+    fill_bg()
+    draw_snake(snake)
+    draw_food(food)
+    pygame.display.update()
+
+
+draw()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+
+    pygame.display.update()
